aee5fafea115983dd0d01c96cd9e855cf7dc0394
saltire sable <saltiresable@gmail.com>
2018-10-05 22:45:30 -0400

    Some random coloured dots and a tank

---
+++ Assets/Scripts/DotScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotScript : MonoBehaviour {
+	public void SetColor(Color color) {
+		GetComponent<SpriteRenderer>().color = color;
+	}
+}
